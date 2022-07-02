0d00ef23e8d23ad961da5a5cb9b35bb1d4dc8589
kaixa <690750353@qq.com>
2022-07-02 23:17:55 +0800

    feat(*): set environment variables
    Use MODE=development in dev mode and only install devtools in dev mode

--- apps/client/src/application.ts
+++ apps/client/src/application.ts
@@ -26,13 +26,17 @@ export class Application {
       Application.checkPlatform();
       Application.registerIpcInvoke();
 
-      import('electron-devtools-installer')
-        .then(installer => {
-          return installer.default(installer.VUEJS3_DEVTOOLS);
-        })
-        .catch(err => {
-          console.error('An error occurred while installing Vue Devtools:', err);
-        });
+      if (__DEV__) {
+        console.log('installing devtools');
+        import('electron-devtools-installer')
+          .then(installer => {
+            return installer.default(installer.VUEJS3_DEVTOOLS);
+          })
+          .catch(err => {
+            console.error('An error occurred while installing Vue Devtools:', err);
+          });
+      }
+
       const window = new MainWindow(preload);
       window.load();
       window.webContents.toggleDevTools();
