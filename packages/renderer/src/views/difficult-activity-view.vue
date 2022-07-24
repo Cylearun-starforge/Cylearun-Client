92ace2827c66b0022ecc9d88701cff8a09cb2760
kaixa <690750353@qq.com>
2022-07-24 20:26:12 +0800

    feat(renderer): difficult activity resources

--- packages/renderer/src/views/difficult-activity-view.vue
+++ packages/renderer/src/views/difficult-activity-view.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import BackButton from '@/components/back-button.vue';
+import BackgroundView from '@/components/difficult-activity/BackgroundView.vue';
 </script>
 
 <template>
+  <background-view />
   <back-button></back-button>
 </template>
 
